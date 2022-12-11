81b05aa13437bb038f6c6a2742eb3ec731fd77cb
Subriti <subritiaryal13@gmail.com>
2022-12-11 21:25:29 +0545

    Working with UI

--- app/src/main/java/com/example/notificationpermissions/DashboardActivity.kt
+++ app/src/main/java/com/example/notificationpermissions/DashboardActivity.kt
@@ -1,20 +1,26 @@
 package com.example.notificationpermissions
 
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toolbar
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager.BackStackEntry
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class DashboardActivity : AppCompatActivity() {
+
+    lateinit var toolbar: androidx.appcompat.widget.Toolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
+
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
         //bottom navigation bar
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
@@ -27,10 +33,28 @@ class DashboardActivity : AppCompatActivity() {
                 R.id.homeFragment, R.id.chatFragment, R.id.historyFragment, R.id.profileFragment
             )
         )
-
         setupActionBarWithNavController(navController, appBarConfiguration)
-
         //mapping ig nav view with fragments
         bottomNavigationView.setupWithNavController(navController)
+
+        //gives back arrow
+        toolbar.setupWithNavController(navController)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.appbar_nav, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.getItemId() == R.id.nav_search){
+            Toast.makeText(this, "Click Search Icon.", Toast.LENGTH_SHORT).show();
+        }
+        else if (item.getItemId() == R.id.nav_notifications){
+            Toast.makeText(this, "Clicked Notifications Icon..", Toast.LENGTH_SHORT).show();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
